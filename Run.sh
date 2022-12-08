1c28aa12ab7890dd26244cfe25009e8a311a4d93
Link_Darck <28543376+Link0Darck@users.noreply.github.com>
2022-12-08 20:44:40 +0100

    Add files via upload

---
+++ Run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# -------------------------------------------------
+# Script qui demarre le BOT
+# -------------------------------------------------
+
+SCREEN="Bot"  #name used for the screen
+
+COMMAND="npx nodemon" #command to start the server (on a raspberry)
+#for the command there are several options (node .), (node index.js), (npx nodemon)
+
+# ------------------------------------------------
+
+echo "Launching the Bot";
+
+screen -AmdS $SCREEN $COMMAND;
+
+exit 0
